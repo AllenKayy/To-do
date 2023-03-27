b9b3be3c8b560c1d4453589031abaa674d736ca4
AllenKayy <allisonoyinkansola1@gmail.com>
2023-03-27 16:06:19 +0100

    added edit option

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-// window.onload = loadTodos;
 
 const todoInput = document.querySelector('input');
 const addTodoBtn = document.querySelector(".addbtn");
-const error = document.querySelector("#error")
+const error = document.querySelector("#error");
 const todoList = document.querySelector("ul");
-const todoCount = document.querySelector("span")
+const todoCount = document.querySelector("span");
 const deleteAllTodo = document.querySelector(".clearAllTodosbtn");
 
-
 // check for stored todos in localStorage
-let todos =JSON.parse(localStorage.getItem("todos")) || [];
+let todos = JSON.parse(localStorage.getItem("todos")) || [];
 
 // render the todos on the page
 renderTodos();
@@ -25,19 +23,12 @@ todoInput.addEventListener("keydown", function(event) {
 
 todoList.addEventListener("click", function(event) {
   if (event.target.tagName === "BUTTON") {
-    deleteTodo(event.target.parentNode);
-  }
-});
-
-todoList.addEventListener("mouseover", function(event) {
-  if (event.target.tagName === "LI") {
-    event.target.firstChild.style.display = "inline-block";
-  }
-});
-
-todoList.addEventListener("mouseout", function(event) {
-  if (event.target.tagName === "LI") {
-    event.target.firstChild.style.display = "none";
+    const li = event.target.parentNode;
+    if (event.target.classList.contains("deleteBtn")) {
+      deleteTodo(li);
+    } else if (event.target.classList.contains("editBtn")) {
+      editTodo(li);
+    }
   }
 });
 
@@ -46,7 +37,7 @@ deleteAllTodo.addEventListener("click", clearTodos);
 // function to add a todo
 function addTodo() {
   const todoText = todoInput.value.trim();
-  error.textContent = " "
+  error.textContent = "";
   if (todoText !== "") {
     const todo = {
       text: todoText,
@@ -54,10 +45,11 @@ function addTodo() {
       date: Date.now()
     };
     todos.unshift(todo);
-    localStorage.setItem("todos", JSON.stringify(todos));  
+    localStorage.setItem("todos", JSON.stringify(todos));
     // update the todo list on the page
     renderTodos();
-    todoInput.value = ""; // reset the input field
+    // reset the input field
+    todoInput.value = ""; 
   } else {
     error.textContent = "Add your Todo! 😒";
   }
@@ -69,37 +61,45 @@ function renderTodos() {
   for (let i = 0; i < todos.length; i++) {
     const todo = todos[i];
     const li = document.createElement("li");
-    const todoText = document.createTextNode(todo.text);
+    li.setAttribute("data-id", i);
+    const todoText = document.createElement("span");
+    todoText.classList.add("todoText");
+    todoText.textContent = todo.text;
+    const buttonContainer = document.createElement("div"); 
+    const editButton = document.createElement("button");
+    editButton.classList.add("editBtn");
+    editButton.innerHTML = '<i class="fas fa-edit"></i>';
     const deleteButton = document.createElement("button");
-    const deleteIcon = document.createElement("i");
-    deleteIcon.className = "fa-solid fa-trash-can";
-    deleteButton.appendChild(deleteIcon);
+    deleteButton.classList.add("deleteBtn");
+    deleteButton.innerHTML = '<i class="fas fa-trash-alt"></i>';
+    buttonContainer.appendChild(editButton); 
+    buttonContainer.appendChild(deleteButton);
     li.appendChild(todoText);
-    li.appendChild(deleteButton);
+    li.appendChild(buttonContainer); 
     todoList.appendChild(li);
 
-    deleteButton.addEventListener("click", function () {
-      todos.splice(i, 1);
-      renderTodos();
-      saveTodos();
+    deleteButton.addEventListener("click", function() {
+      deleteTodo(li);
+    });
+
+    editButton.addEventListener("click", function() {
+      editTodo(li);
     });
   }
   updateTodoCount();
 }
 
-/**
- * // function to delete a todo
+// function to delete a todo
 function deleteTodo(todoItem) {
-  const index = Array.from(todoList.children).indexOf(todoItem);
+  const index = parseInt(todoItem.getAttribute("data-id"));
   // remove the todo from the list
-  todos.splice(index, 1); 
+  todos.splice(index, 1);
   // update localStorage
-  localStorage.setItem("todos", JSON.stringify(todos)); 
+  localStorage.setItem("todos", JSON.stringify(todos));
   // remove the todo from the page
-  todoItem.remove(); 
+  todoItem.remove();
   updateTodoCount();
-} 
-**/
+}
 
 // function to clear all todos
 function clearTodos() {
@@ -107,12 +107,25 @@ function clearTodos() {
   // update localStorage
   localStorage.setItem("todos", JSON.stringify(todos));
   // update the todo list on the page
-  renderTodos(); 
+  renderTodos();
 }
 
 // function to update the todo count in the footer
 function updateTodoCount() {
   const count = todos.length;
   todoCount.textContent = count;
+}
+
+// function to show edit todo form
+function editTodo(todoItem) {
+  const index = Array.from(todoList.children).indexOf(todoItem);
+  const todo = todos[index];
+  const todoText = todo.text;
+  const newTodoText = prompt("Edit todo", todoText);
+  if (newTodoText !== null && newTodoText.trim() !== "") {
+    todo.text = newTodoText.trim();
+    localStorage.setItem("todos", JSON.stringify(todos));
+    renderTodos();
+  }
 }
 
